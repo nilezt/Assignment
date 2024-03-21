2b068cbbe34ea00f39c1a0395210beb85c51c808
Tze Lin <nilezt@hotmail.com>
2024-03-22 00:09:33 +0800

    First commit

---
+++ Module4Task1.py
@@ -0,0 +1,12 @@
+import math
+r = 5
+Area =  math.pi * pow(r,2)
+print("Area size = ", Area)
+
+
+Area =  math.pi * r**2
+print("Area size = ", Area)
+
+
+Circumference  = 2 * math.pi * r 
+print("Circumference =", Circumference)
